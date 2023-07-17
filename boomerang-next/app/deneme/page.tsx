0d5e96b007dbd3c1ab4516b9eb7d37fc4165a57a
ergen4006 <brhmrgn1@gmail.com>
2023-07-18 02:07:03 +0300

    recordrtc is added

--- boomerang-next/app/deneme/page.tsx
+++ boomerang-next/app/deneme/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { Hero } from "@/components/common";
+import { useRecorderPermission } from "@/components/common/useRecorderPermission";
 import LessonPart from "@/components/pages/denemeler/LessonPart";
 import TestPartGenelDeneme from "@/components/pages/denemeler/TestPartGenelDeneme";
 import { useState, useRef, useEffect } from "react";
+import { invokeSaveAsDialog } from "recordrtc";
 
 const GenelDeneme = () => {
+  const recorder = useRecorderPermission("canvas");
+
+  const startRecording = async () => {
+    recorder.startRecording();
+  };
+
+  const stopRecording = async () => {
+    await recorder.stopRecording();
+    let blob = await recorder.getBlob();
+    invokeSaveAsDialog(blob, `random_name.webm`);
+  };
   const [isTimerOn, setIsTimerOn] = useState(false);
 
   const [studentAnswer, setStudentAnswer] = useState({});
@@ -50,7 +63,11 @@ const GenelDeneme = () => {
 
   return (
     <main>
-      <Hero pageSubject="MİNİ DENEME" barTitle="Mini Deneme"/>
+      <Hero pageSubject="MİNİ DENEME" barTitle="Mini Deneme" />
+      {/* <div>
+        <button onClick={startRecording}> Start recording</button>
+        <button onClick={stopRecording}> Stop recording</button>
+      </div> */}
       <LessonPart
         setActiveLesson={setActiveLesson}
         studentAnswer={studentAnswer}
